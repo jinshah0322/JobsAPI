4483153d313314f4d9da21e0dd1dab92e6dc85af
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2023-06-27 15:23:03 +0530

    duplicated email error in error-handler.js

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,19 @@
 const CustomAPIError = require('../errors/custom-error')
 const StatusCodes = require('http-status-codes')
 const errorHandler = (err, req, res, next) => {
+  let customError = {
+    statusCode: err.statusCode || 500,
+    msg:err.message || "Something went wrong try again later."
+  }
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message, success:false })
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  if(err.code && err.code === 11000){
+    customError.msg = `Duplicate value entered for email field, please choose another value`
+    customError.statusCode = 400
+  }
+  // return res.status(400).send({err})
+  return res.status(customError.statusCode).json({ msg:customError.msg,success:false})
 }
 
 module.exports = errorHandler
