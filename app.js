e834f41c58058ca701b707a1fa2a3f46940f46df
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2023-06-27 19:16:32 +0530

    added security using helmet,cors,xss-clean and express-rate-limit

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
 require('express-async-errors');
+
+//extra security
+const helmet = require("helmet")
+const cors = require("cors")
+const xss = require("xss-clean")
+const ratelimiter = require("express-rate-limit")
+
+
 const express = require('express');
 const app = express();
 const mongoose = require("mongoose")
@@ -15,7 +23,13 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
-// extra packages
+app.use(ratelimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+}))
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
 app.use("/api/v1/auth",authRoute)
